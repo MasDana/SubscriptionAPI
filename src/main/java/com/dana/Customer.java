87abdb11d046f6c118a0ca4bff99f2dca35fe0d3
MasDana <putudeni1414@gmail.com>
2024-06-14 13:57:09 +0800

    First change : customer tambah baryu

---
+++ src/main/java/com/dana/Customer.java
@@ -0,0 +1,10 @@
+package com.dana;
+public class Customer {
+    private int id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+}
+
---
+++ src/main/java/com/dana/subscriptionItems.java
@@ -0,0 +1,4 @@
+package com.dana;
+
+public class subscriptionItems {
+}
