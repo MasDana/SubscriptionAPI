37c9e2a46702ac53c4b991e1ca5a96260c9651ac
MasDana <putudeni1414@gmail.com>
2024-06-15 17:40:59 +0800

    Tambahan pada Class shippingAddresses

--- src/main/java/com/dana/shippingAddresses.java
+++ src/main/java/com/dana/shippingAddresses.java
@@ -1,14 +1,79 @@
 package com.dana;
-CREATE TABLE `shipping_addresses` (
-        `id` INTEGER PRIMARY KEY,
-        `customer` INTEGER NOT NULL,
-        `title` TEXT NOT NULL, /* nama dari alamat, misalnya: rumah, kantor, rumah 2*/
-        `line1` TEXT NOT NULL,
-        `line2` TEXT,
-        `city` TEXT NOT NULL,
-        `province` TEXT NOT NULL,
-        `postocde` TEXT NOT NULL
-);
+
 public class shippingAddresses {
 
+
+    private int id;
+    private int customer;
+    private String title;
+    private String line1;
+    private String line2;
+    private String city;
+    private String province;
+    private String postcode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer() {
+        this.customer = customer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle() {
+        this.title = title;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1() {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2() {
+        this.line2 = line2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity() {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince() {
+        this.province = province;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode() {
+        this.postcode = postcode;
+    }
 }
+
