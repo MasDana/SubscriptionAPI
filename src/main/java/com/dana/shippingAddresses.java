65bb5c6f89e0d5993817efd8c4423bf95da3537f
MasDana <putudeni1414@gmail.com>
2024-06-15 17:28:54 +0800

    Class shippingAddresses baru

--- src/main/java/com/dana/shippingAddresses.java
+++ src/main/java/com/dana/shippingAddresses.java
@@ -1,4 +1,14 @@
 package com.dana;
-
+CREATE TABLE `shipping_addresses` (
+        `id` INTEGER PRIMARY KEY,
+        `customer` INTEGER NOT NULL,
+        `title` TEXT NOT NULL, /* nama dari alamat, misalnya: rumah, kantor, rumah 2*/
+        `line1` TEXT NOT NULL,
+        `line2` TEXT,
+        `city` TEXT NOT NULL,
+        `province` TEXT NOT NULL,
+        `postocde` TEXT NOT NULL
+);
 public class shippingAddresses {
+
 }
