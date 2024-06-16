337e258a6b8d5e670bb4dbcd14d937fffa825e75
MasDana <putudeni1414@gmail.com>
2024-06-17 01:54:16 +0800

    Menambah metode objectJSON dan parse untuk item

--- src/main/java/com/dana/Items.java
+++ src/main/java/com/dana/Items.java
@@ -1,5 +1,8 @@
 package com.dana;
-
+import org.json.JSONObject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 public class Items {
 
     private int id;
@@ -27,5 +30,28 @@ public class Items {
     public int getIsActive(){return isActive;}
 
     public void setIsActive(){this.isActive = isActive;}
+
+    public JSONObject objectJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", id);
+        jsonObject.put("product_name", name);
+        jsonObject.put("price", price);
+        jsonObject.put("type", type);
+        jsonObject.put("is_active", isActive);
+        return jsonObject;
+    }
+
+    public int itemsParse(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            name = obj.getString("name");
+            price = obj.getInt("price");
+            type = obj.getString("type");
+            isActive = obj.getInt("is_active");
+        } catch (Exception e) {
+            return 1;
+        }
+        return 0;
+    }
 
 }
