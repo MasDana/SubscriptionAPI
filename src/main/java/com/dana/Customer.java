f49439426afc8d35f93830a14276f3e70c96ed52
MasDana <putudeni1414@gmail.com>
2024-06-17 01:03:14 +0800

    Mencoba tambah metode objectJSON dan parse untuk customer

--- src/main/java/com/dana/Customer.java
+++ src/main/java/com/dana/Customer.java
@@ -1,10 +1,16 @@
 package com.dana;
+
+import org.json.JSONObject;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 public class Customer {
     private int id;
     private String email;
     private String firstName;
     private String lastName;
     private String phoneNumber;
+
     public int getId() {
         return id;
     }
@@ -44,5 +50,30 @@ public class Customer {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    public JSONObject objectJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("users", id);
+        jsonObject.put("first_name", firstName);
+        jsonObject.put("last_name", lastName);
+        jsonObject.put("email", email);
+        jsonObject.put("phone_number", phoneNumber);
+        return jsonObject;
+    }
+
+    public int customerParse(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            firstName = obj.getString("first_name");
+            lastName = obj.getString("last_name");
+            email = obj.getString("email");
+            phoneNumber = obj.getString("phone_number");
+        } catch (Exception e) {
+            return 1;
+        }
+        return 0;
+    }
+
+
 }
 
