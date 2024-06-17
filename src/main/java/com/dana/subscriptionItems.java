a38b6bcfbdf868c4ba833cc8d7c53ca44795da4d
MasDana <putudeni1414@gmail.com>
2024-06-17 15:49:35 +0800

    Menambah fungsi update card dan insert card

--- src/main/java/com/dana/subscriptionItems.java
+++ src/main/java/com/dana/subscriptionItems.java
@@ -1,4 +1,7 @@
 package com.dana;
+
+import org.json.JSONObject;
+
 public class subscriptionItems {
 
     private int subscription;
@@ -7,24 +10,68 @@ public class subscriptionItems {
     private  int price;
     private int amount;
 
-    public int getSubscription(){return subscription;}
+    public int getSubscription(){
+        return subscription;
+    }
 
-    public void setSubscription(){this.subscription = subscription;}
+    public void setSubscription(){
+        this.subscription = subscription;
+    }
 
-    public int getItem(){return item;}
+    public int getItem(){
+        return item;
+    }
 
-    public void setItem(){this.item = item;}
+    public void setItem(){
+        this.item = item;
+    }
 
-    public int getQuantity(){return quantity;}
+    public int getQuantity(){
+        return quantity;
+    }
 
-    public void setQuantity(){this.price = quantity;}
+    public void setQuantity(){
+        this.price = quantity;
+    }
 
-    public int getPrice(){return price;}
+    public int getPrice(){
+        return price;
+    }
 
-    public void setPrice(){this.price = price;}
+    public void setPrice(){
+        this.price = price;
+    }
 
-    public int getAmount(){return amount;}
+    public int getAmount(){
+        return amount;
+    }
 
-    public void setAmount(){this.amount = amount;}
+    public void setAmount(){
+        this.amount = amount;
+    }
+
+    public JSONObject objectJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("subscription", subscription);
+        jsonObject.put("item", item);
+        jsonObject.put("quantity", quantity);
+        jsonObject.put("price", price);
+        jsonObject.put("amount", amount);
+        return jsonObject;
+    }
+
+    public int subscriptionItemParsing(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            subscription = obj.getInt("subscription");
+            item = obj.getInt("item");
+            quantity = obj.getInt("quantity");
+            price = obj.getInt("price");
+            amount = obj.getInt("amount");
+        } catch (Exception e) {
+            return 1;
+        }
+        return 0;
+    }
 
 }
