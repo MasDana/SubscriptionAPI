ebe91591940e09d4d561364343179418c680a474
MasDana <putudeni1414@gmail.com>
2024-06-17 22:58:56 +0800

    Menambah metode objectjson dna shippingAddressesParsing

--- src/main/java/com/dana/shippingAddresses.java
+++ src/main/java/com/dana/shippingAddresses.java
@@ -1,8 +1,8 @@
 package com.dana;
 
+import org.json.JSONObject;
+
 public class shippingAddresses {
-
-
     private int id;
     private int customer;
     private String title;
@@ -74,6 +74,35 @@ public class shippingAddresses {
 
     public void setPostcode() {
         this.postcode = postcode;
+    }
+
+    public JSONObject objectJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("user", id);
+        jsonObject.put("customer", customer);
+        jsonObject.put("title", title);
+        jsonObject.put("line1", line1);
+        jsonObject.put("line2", line2);
+        jsonObject.put("city", city);
+        jsonObject.put("province", province);
+        jsonObject.put("postcode", postcode)
+        return jsonObject;
+    }
+
+    public int shippingAddressParsing(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            customer= obj.getInt("customer");
+            title = obj.getString("title");
+            line1 = obj.getString("line1");
+            line2 = obj.getString("line2");
+            city = obj.getString("city");
+            province = obj.getString("province");
+            postcode = obj.getString("postcode");
+        } catch (Exception e) {
+            return 1;
+        }
+        return 0;
     }
 }
 
