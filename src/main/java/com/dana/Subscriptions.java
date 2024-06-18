74acff8a5154085b44dc214f5c4f08f7e45e0816
MasDana <putudeni1414@gmail.com>
2024-06-18 14:48:19 +0800

    Membuat objectjson dan subscriptionsParsing

--- src/main/java/com/dana/Subscriptions.java
+++ src/main/java/com/dana/Subscriptions.java
@@ -1,5 +1,7 @@
 package com.dana;
 
+import org.json.JSONObject;
+
 public class Subscriptions {
     private int id;
     private int customer;
@@ -82,4 +84,37 @@ public class Subscriptions {
     public void setStatus(String status){
         this.status = status;
     }
+
+    public JSONObject objectJSON() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", id);
+        jsonObject.put("customer", customer);
+        jsonObject.put("billing_period", billingPeriod);
+        jsonObject.put("billing_period_unit", billingPeriodUnit);
+        jsonObject.put("total_due", totalDue);
+        jsonObject.put("activated_at",activatedAt);
+        jsonObject.put("current_term_start",currentTermStart);
+        jsonObject.put("current_term_end", currentTermEnd);
+        jsonObject.put("status", status);
+        return jsonObject;
+    }
+
+    public int subscriptionsParsing(String json) {
+        try {
+            JSONObject obj = new JSONObject(json);
+            id = obj.getInt("id");
+            customer = obj.getInt("customer");
+            billingPeriod = obj.getInt("billing_period");
+            billingPeriodUnit = obj.getString("billing_period_unit");
+            totalDue = obj.getInt("total_due");
+            activatedAt = obj.getString("activated_at");
+            currentTermStart = obj.getString("current_term_start");
+            currentTermEnd = obj.getString("current_term_end");
+            status = obj.getString("status");
+        } catch (Exception e) {
+            return 1;
+        }
+        return 0;
+    }
+
 }
