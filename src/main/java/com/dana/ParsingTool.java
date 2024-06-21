4c3d71b1025be0ee38a00f03862302a06e9f5170
MasDana <putudeni1414@gmail.com>
2024-06-21 16:05:20 +0800

    Memperbaharui class parsingtool

--- src/main/java/com/dana/ParsingTool.java
+++ src/main/java/com/dana/ParsingTool.java
@@ -6,7 +6,16 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParsingTool {
+
+    public static <T> T jsonParsing(String json, Class<T> valueType) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.readValue(json, valueType);
+    }
+
     public static JsonNode jsonParsing(String json) throws JsonProcessingException {
         ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.readTree(json);
@@ -15,6 +24,7 @@ public class ParsingTool {
     public static String[] pemisahString(String text, String delimiter) {
         return Arrays.stream(text.split(delimiter)).filter(value -> value != null && value.length() > 0).toArray(String[]::new);
     }
+
     public static String parseInputStream(InputStream inputStream) throws IOException {
         InputStreamReader inputStreamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
         BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
@@ -30,16 +40,21 @@ public class ParsingTool {
 
         return buffer.toString();
     }
-    public static String queryParsing(String requestQuery) {
-        // Pisahkan query string menjadi pasangan kunci-nilai menggunakan "&"
-        String[] queries = requestQuery.split("&");
 
-        // Jika query memiliki 3 bagian, asumsikan format field, condition, value
-        if (queries.length == 3) {
-            String field = "";
-            String condition = "";
-            String value = "";
-            try {
+    public static String queryParsing(String requestQuery) {
+        if (requestQuery == null || requestQuery.isEmpty()) {
+            return null;
+        }
+
+        try {
+            String[] queries = requestQuery.split("&");
+
+            // Jika query memiliki 3 bagian, asumsikan format field, condition, value
+            if (queries.length == 3) {
+                String field = "";
+                String condition = "";
+                String value = "";
+
                 for (String query : queries) {
                     String[] queryParts = query.split("=");
                     if (queryParts.length != 2) {
@@ -72,6 +87,12 @@ public class ParsingTool {
                                 case "notEqual":
                                     condition = "<>";
                                     break;
+                                case "true":
+                                    condition = "1"; // Mengubah true menjadi 1
+                                    break;
+                                case "false":
+                                    condition = "0"; // Mengubah false menjadi 0
+                                    break;
                                 default:
                                     return null;
                             }
@@ -84,26 +105,42 @@ public class ParsingTool {
                     }
                 }
                 return field + " " + condition + " " + value;
-            } catch (Exception e) {
-                e.printStackTrace();
             }
-        }
-        // Jika query hanya memiliki 1 bagian, asumsikan format key=value
-        else if (queries.length == 1) {
-            try {
+            // Jika query hanya memiliki 1 bagian, asumsikan format key=value
+            else if (queries.length == 1) {
                 String[] queryParts = queries[0].split("=");
                 if (queryParts.length != 2) {
                     return null;
                 }
                 String key = queryParts[0];
                 String value = queryParts[1];
-                return key + " = \"" + value + "\"";
-            } catch (Exception e) {
-                e.printStackTrace();
+
+                // Memeriksa apakah value adalah "true" atau "false"
+                if (value.equalsIgnoreCase("true")) {
+                    return key + " = 1"; // Mengembalikan "isActive = 1"
+                } else if (value.equalsIgnoreCase("false")) {
+                    return key + " = 0"; // Mengembalikan "isActive = 0"
+                } else {
+                    return key + " = \"" + value + "\""; // Mengembalikan "isActive = value"
+                }
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Atau, bisa juga return null untuk menangani kondisi tidak valid dengan lebih baik
+            return null;
         }
 
         return null;
     }
 
+
+    public static Map<String, String> parseQuery(String query) {
+        Map<String, String> queryPairs = new HashMap<>();
+        String[] pairs = query.split("&");
+        for (String pair : pairs) {
+            int idx = pair.indexOf("=");
+            queryPairs.put(pair.substring(0, idx), pair.substring(idx + 1));
+        }
+        return queryPairs;
+    }
 }
