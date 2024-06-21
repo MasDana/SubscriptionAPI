781f33d4e74d5eda1966d9ce36f6dd5aab3381cf
MasDana <putudeni1414@gmail.com>
2024-06-21 17:04:33 +0800

    shipping address update

---
+++ src/main/java/com/dana/ShippingAddresses.java
@@ -0,0 +1,78 @@
+package com.dana;
+
+public class ShippingAddresses {
+    private int id;
+    private int customer;
+    private String title;
+    private String line1;
+    private String line2;
+    private String city;
+    private String province;
+    private String postcode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer() {
+        this.customer = customer;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle() {
+        this.title = title;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1() {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2() {
+        this.line2 = line2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity() {
+        this.city = city;
+    }
+
+    public String getProvince() {
+        return province;
+    }
+
+    public void setProvince() {
+        this.province = province;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public void setPostcode() {
+        this.postcode = postcode;
+    }
+
+}
+
